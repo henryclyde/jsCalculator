200e5369d1855b200444fa2afe1c9fe7b921d89f
Henry Clyde <22669531+henryclyde@users.noreply.github.com>
2024-04-21 01:07:19 -0600

    crwated operator functions

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-
-
+function add(n1,n2) {
+    return n1+n2;
+}
+function subtract(n1,n2) {
+    return n1-n2;
+}
+function multiply(n1,n2) {
+    return n1 * n2;
+}
+function divide(n1,n2) {
+    return n1 / n2;
+}
 let n1,op,n2;
 
 function operate(n1,op,n2) {
 
 }
+
+function solution() {
+
+}
+let divs = [...document.querySelectorAll('div')].filter(div => div.parentNode.classList.contains('buttons'));
+divs.forEach(item in divs)
